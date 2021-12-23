a8cf4c60e0cd78c0501634fabe952dd48140b08a
jahidul <programmerjahid162@gmail.com>
2021-12-23 17:03:16 +0600

    solve catagory problem

--- src/Component/SortByCatagory/SortByCatagory.js
+++ src/Component/SortByCatagory/SortByCatagory.js
@@ -1,46 +1,58 @@
 import React, { useEffect, useState } from "react";
+import Catagories from "./catagories";
 
 const SortByCatagory = () => {
-  const [user, setUser] = useState([]);
+  // const [user, setUser] = useState(Catagories);
 
+  const [user, setUser] = useState(Catagories);
 
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setUser(data);
+  //       // console.log(data);
+  //       // console.log(data);
+  //     });
+  // }, []);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setUser(data);
-        // console.log(data);
-        // console.log(data);
-      });
-  }, []);
+  const filterResult = (catItem) => {
+    // console.log("test",user);
 
-
-  const filterResult = () => {
-  
-    console.log("test",user);
-
+    // console.log("user data ", user);
+    const result = Catagories.filter((curdata) => {
+      return curdata.category === catItem;
+    });
+    setUser(result);
   };
 
   return (
     <div className="row mt-5">
       <div className="col-md-4  ps-5">
-        <button
+   
+      <button
           className="btn btn-warning w-75 my-3 "
-          onClick={() => filterResult("Gwenborough")}
+          onClick={() => setUser(Catagories)}
         >
           {" "}
-          Clementine Bauch
+          All
+        </button>
+        <button
+          className="btn btn-warning w-75 my-3 "
+          onClick={() => filterResult("Women")}
+        >
+          {" "}
+          Wo-Mens
         </button>
         <button
           className="btn btn-warning w-75"
           onClick={() => {
             console.log("press");
-            filterResult("Wisokyburgh");
+            filterResult("Men");
           }}
         >
           {" "}
-          Patricia Lebsack
+          Mens
         </button>
       </div>
 
@@ -56,8 +68,7 @@ const SortByCatagory = () => {
               padding: 30,
             }}
           >
-            <h4> {us.name} </h4>
-            <p>{us.address.city}</p>
+            <h4> {us.title} </h4>
           </div>
         ))}
       </div>
--- src/Component/SortByCatagory/catagories.js
+++ src/Component/SortByCatagory/catagories.js
@@ -1,26 +1,23 @@
 const Catagories = [
     {
-        "id": 1,
-        "name": "Leanne Graham",
-        "username": "Bret",
-        "email": "Sincere@april.biz",
-        "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874",
-        "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-        }
-        },
-        "phone": "1-770-736-8031 x56442",
-        "website": "hildegard.org",
-        "company": {
-        "name": "Romaguera-Crona",
-        "catchPhrase": "Multi-layered client-server neural-net",
-        "bs": "harness real-time e-markets"
-        }  
+      
+        id : 1,
+        title : "Mens Black Shirt",
+        price : 213,
+        category: 'Men',
+        images :"https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+
+        
+    }
+    ,
+    {
+      
+        id : 2,
+        title : "Wo-Mens Black Shirt",
+        price : 213,
+        category: 'Women',
+        images :"https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+
     }
 ]
 
